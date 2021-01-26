2acd24832b845aa098f0934c9af52bfe17effb10
kristofre <renders.kristof@gmail.com>
2021-01-26 17:38:42 +0100

    update monaco to v1.2.0

--- build-env.sh
+++ build-env.sh
@@ -1,7 +1,7 @@
 ##########################################
 #  VARIABLES                             #
 ##########################################
-monaco_version="v1.1.0" 
+monaco_version="v1.2.0" 
 source_repo="https://github.com/dynatrace-ace/perform2021-vhot-monaco" 
 clone_folder="bootstrap"
 domain="nip.io"
